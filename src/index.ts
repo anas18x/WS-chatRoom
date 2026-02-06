4f3f69f67cbeba8074404d0ed5db18b5d3b29934
anas18x <anasshakil00@gmail.com>
2026-02-06 22:31:11 +0530

    :sparkles: FEATURE : room-based chat functionality

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,49 @@
 import { ENV } from "./config/env.config.js";
-import { WebSocketServer , WebSocket} from "ws";
-const wss = new WebSocketServer({port : ENV.PORT as number}, () => {
-    console.log(`WebSocket server running on port ${ENV.PORT}`)
-})
+import { WebSocketServer, WebSocket } from "ws";
+const wss = new WebSocketServer({ port: ENV.PORT as number }, () => {
+  console.log(`WebSocket server running on port ${ENV.PORT}`);
+});
 
-
-let clients : WebSocket[] = []
+let clients = new Map<string, Set<WebSocket>>();
 
 // socket is the client that is connecting to the server, it lets us send and receive messages
-wss.on('connection',(socket: WebSocket)=>{
-        clients.push(socket)
-     console.log("client connected to the server")
-     socket.send("Welcome to the WebSocket server!")
+wss.on("connection", (socket) => {
+  console.log("client connected to the server");
 
-     socket.on('message',(message)=>{
-        console.log(`message Received from client : ${message.toString()}`)
-
-        // Broadcast the message to all connected clients
-        clients.forEach((client)=>{
-            client.send(`Message from client : ${message.toString()}`)
-        })
-     })
+  socket.on("message", (message) => {
+    const parsedMessage = JSON.parse(message.toString());
 
 
-})
+    if (parsedMessage.type === "join") {
+      if (!clients.has(parsedMessage.roomId)) {
+        clients.set(parsedMessage.roomId, new Set());
+      }
+
+      // add socket to the room
+      clients.get(parsedMessage.roomId)?.add(socket);
+      console.log(
+        `${parsedMessage.name} joined the room ${parsedMessage.roomId}`,
+      );
+    }
+
+
+    if (parsedMessage.type === "chat") {
+      const currentUserRoom = clients.get(parsedMessage.roomId);
+      currentUserRoom?.forEach((clientSocket) => {
+        clientSocket.send(parsedMessage.payload.message);
+      });
+    }
+
+  });
+
+
+  socket.on("close", () => {
+    clients.forEach((sockets, roomId) => {
+      if (sockets.has(socket)) {
+        sockets.delete(socket);
+        console.log(`client left the room ${roomId}`);
+      }
+    });
+  });
+  
+});
